eacb05b0a9084df60d34ea53c8eb67ea0457541a
Keita <fmb.vb.1996@gmail.com>
2022-02-18 16:47:54 +0900

    コード修正

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Support\Facades\Storage;
 
 class RegisterController extends Controller
 {
