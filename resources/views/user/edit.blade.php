d19553fff2b3febab09422a45af6f6b0939a3d93
Keita <fmb.vb.1996@gmail.com>
2022-03-21 15:41:17 +0900

    会員編集画面追加

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('edit') }}</div>
+                <div class="card-header">プロフィール編集</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ url('/update/'.$auth->id) }}" enctype="multipart/form-data">
