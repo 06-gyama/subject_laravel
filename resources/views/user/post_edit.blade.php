cfdf37406e960ebe5e0cbfdcfe196709d4cd21e4
Keita <fmb.vb.1996@gmail.com>
2022-03-21 15:02:41 +0900

    投稿編集画面追加

--- resources/views/user/post_edit.blade.php
+++ resources/views/user/post_edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('post_edit') }}</div>
+                <div class="card-header">投稿編集</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ url('/post_update') }}" enctype="multipart/form-data">
